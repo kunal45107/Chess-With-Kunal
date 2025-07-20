d430a97872a5b921d02f52d0e16231510762c22f
Kunal_Keshri <kunalkeshri1490@gmail.com>
2025-07-20 11:48:08 +0530

    Add mobile touch support for pieces

--- JS/chess.js
+++ JS/chess.js
@@ -14,3 +14,22 @@ document.querySelectorAll('.white-square, .black-square').forEach(square =>
         square.appendChild(piece);
     }
 );
+
+let tappedPiece = null;
+
+document.querySelectorAll('[draggable]').forEach(piece => {
+    piece.addEventListener('touchstart', () => {
+        tappedPiece = piece;
+    });
+});
+
+document.querySelectorAll('.white-square, .black-square').forEach(square => {
+    square.addEventListener('touchstart', () => {
+        if (tappedPiece) {
+            const oldPiece = square.querySelector('img.chess_pieces');
+            if (oldPiece) oldPiece.remove();
+            square.appendChild(tappedPiece);
+            tappedPiece = null;
+        }
+    });
+});
