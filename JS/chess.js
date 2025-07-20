c7680c5114816bcbf5d467cbf884198ee2532a1d
Kunal_Keshri <kunalkeshri1490@gmail.com>
2025-07-20 12:01:03 +0530

    Remove mobile touch support for chess pieces

--- JS/chess.js
+++ JS/chess.js
@@ -15,21 +15,3 @@ document.querySelectorAll('.white-square, .black-square').forEach(square =>
     }
 );
 
-let selectedPiece = null;
-
-document.querySelectorAll('img.chess_pieces').forEach(piece => {
-    piece.addEventListener('click', () => {
-        selectedPiece = piece;
-    });
-});
-
-document.querySelectorAll('.white-square, .black-square').forEach(square => {
-    square.addEventListener('click', () => {
-        if (selectedPiece) {
-            const oldPiece = square.querySelector('img.chess_pieces');
-            if (oldPiece) oldPiece.remove();
-            square.appendChild(selectedPiece);
-            selectedPiece = null;
-        }
-    });
-});
